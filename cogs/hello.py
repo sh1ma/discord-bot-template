7f4d50024ad59cae4d64b96cbd51308892ea23f6
sh1ma <in9lude@gmail.com>
2020-09-06 17:28:57 +0900

    hello

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,14 +1,14 @@
 from discord.ext.commands import Bot, Cog, Context, command
 
 
-class Echo(Cog):
+class Hello(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
-    @command
+    @command()
     def hello(self, ctx: Context):
         ctx.send("hello")
 
 
 def setup(bot: Bot) -> None:
-    bot.add_cog(Echo(bot))
+    bot.add_cog(Hello(bot))
